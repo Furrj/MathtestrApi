f002f1146eff015225823ba9c63641e87df8346a
Jackson Furr <jackson.a.furr@gmail.com>
2023-09-15 02:58:53 -0400

    updated init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,34 @@
-CREATE TABLE account_info (
+CREATE DATABASE mathtestr;
+
+/c mathtestr
+
+CREATE TABLE user_info (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(50)
+    username VARCHAR(16),
+    password VARCHAR(16),
+    first_name VARCHAR(16),
+    last_name VARCHAR(16)
 );
 
-INSERT INTO account_info (username, password)
-VALUES ('Jackson', 'Pass');
+CREATE TABLE session_data(
+    user_id INTEGER,
+    uuid VARCHAR(36),
+    expires BIGINT,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES user_info(user_id)
+);
+
+INSERT INTO user_info (username, password, first_name, last_name)
+VALUES ('poemmys', 'pad89!', 'Jackson', 'Furr');
+
+SELECT * FROM user_info;
+
+INSERT INTO session_data (user_id, uuid, expires)
+VALUES (0, 'test_uuid', 1234);
+
+SELECT * FROM session_data;
+
+SELECT * FROM user_info
+INNER JOIN session_data
+USING (user_id);
