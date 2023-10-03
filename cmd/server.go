b9bed57ac5fbcba81205702b5dc7b34f585958a1
Jackson Furr <jackson.a.furr@gmail.com>
2023-10-03 04:36:32 -0400

    added spa

--- cmd/server.go
+++ cmd/server.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/mandrigin/gin-spa/spa"
 	"mathtestr.com/server/internal/dbHandling"
 	"mathtestr.com/server/internal/routeHandling"
 )
 
 func main() {
+	// ENV CONFIG
 	if os.Getenv("MODE") != "PROD" {
 		godotenv.Load("../config.env")
 	}
@@ -26,25 +28,10 @@ func main() {
 	}
 	fmt.Printf("%+v\n", user)
 
+	// ROUTING
 	routeHandler := routeHandling.InitRouteHandler(dbHandler)
 	router := gin.Default()
 	router.POST("/register", routeHandler.Register)
+	router.Use(spa.Middleware("/", "client"))
 	router.Run(":5000")
-
-	// userList, err := dbHandlers.GetAllUsers(db)
-	// if err != nil {
-	// 	fmt.Printf("%+v\n", err)
-	// }
-	// for _, user := range userList {
-	// 	fmt.Printf("%+v\n", user)
-	// }
-
-	// // Routing
-	// router := gin.Default()
-
-	// router.POST("/login", routeHandlers.LoginPost(db))
-	// router.POST("/register", routeHandlers.RegisterPost(db))
-	// router.Use(spa.Middleware("/", "client"))
-
-	// router.Run(":5000")
 }
