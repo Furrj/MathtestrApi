7e183424215ee91b110dca6a7d4739abe293956d
Jackson Furr <jackson.a.furr@gmail.com>
2023-11-12 03:37:56 -0500

    created test_result sql table

--- init.sql
+++ init.sql
@@ -19,6 +19,18 @@ CREATE TABLE session_data(
             REFERENCES user_info(user_id)
 );
 
+CREATE TABLE test_results(
+    user_id INTEGER,
+    score SMALLINT,
+    min INTEGER,
+    max INTEGER,
+    question_count SMALLINT,
+    operations VARCHAR(50),
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id)
+            REFERENCES user_info(user_id)
+);
+
 INSERT INTO user_info (username, password, first_name, last_name)
 VALUES ('a', 'pad89!', 'Jackson', 'Furr');
 
@@ -29,6 +41,11 @@ VALUES (0, 'test_uuid', 1234);
 
 SELECT * FROM session_data;
 
+INSERT INTO test_results(user_id, score, min, max, question_count, operations)
+VALUES (3, 100, 1, 12, 5, 'Multiplication');
+
 SELECT * FROM user_info
 INNER JOIN session_data
+USING (user_id)
+INNER JOIN test_results
 USING (user_id);
